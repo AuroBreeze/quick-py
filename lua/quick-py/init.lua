4a18314a451ca025e7c4901b37116badc295ab04
AuroBreeze <1732373074@qq.com>
2025-05-25 14:51:05 +0800

    fix(lua/quick-py): 修复运行 Python 文件时终端快速闪退问题
    - 使用 vim.defer_fn 延迟执行 betterTerm.open 函数
    - 确保在 Neovim 界面更新后打开终端
    - 解决了在 Windows 系统下运行时终端窗口快速闪退的问题

--- lua/quick-py/init.lua
+++ lua/quick-py/init.lua
@@ -163,7 +163,9 @@ vim.api.nvim_create_user_command('RunPython', function()
     local cmd = config.python_path .. ' ' .. vim.fn.shellescape(vim.fn.expand('%:p'))
     local ok2, betterTerm = pcall(require, 'betterTerm')
     if ok2 then 
-        betterTerm.send(cmd,1)
+        vim.defer_fn(function()
+            betterTerm.open(cmd)
+        end, 100)
     else vim.cmd('!' .. cmd) end
 end, { desc = 'Run current Python file in virtualenv' })
 
